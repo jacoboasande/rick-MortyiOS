c0e206f2b2debee03ce9383b7085e798b6611f66
Jacobo Adrian Sande Veiga <sande.jacoboa@gmail.com>
2023-03-11 16:59:30 +0100

    InitialCommit
    Innitial Commit of the project creation

---
+++ code/Rick&Morty/Rick&Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by Jacobo Adrián Sande Veiga on 11/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
